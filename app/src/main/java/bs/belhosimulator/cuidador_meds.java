902b028bde9166abe05b67877418e823b456f875
Nuno Caridade <32111888+Karite@users.noreply.github.com>
2019-04-06 03:25:11 +0100

    comite crl

---
+++ app/src/main/java/bs/belhosimulator/cuidador_meds.java
@@ -0,0 +1,13 @@
+package bs.belhosimulator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+public class cuidador_meds extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cuidador_meds);
+    }
+}
